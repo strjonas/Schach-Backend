94612e3aaa89640da43e00a7ac2f62158144dd97
madpascal93 <96667889+madpascal93@users.noreply.github.com>
2023-04-27 15:56:12 +0200

    King class proto

--- ChessProjectPlayground/demo/src/main/java/com/example/demo/Pieces/King.java
+++ ChessProjectPlayground/demo/src/main/java/com/example/demo/Pieces/King.java
@@ -1,16 +1,22 @@
 package com.example.demo.Pieces;
 
 public class King extends Pieces{
+    private boolean canCastleQ;
+    private boolean canCastleK;
 
+    public boolean isMoveValid(int posY, int posX, int newPosY, int newPosX, Board board){
+        return super.isMoveValid() && isKingMovement(posY, posX, newPosY, newPosX) && isSomethingInTheWay(newPosY, newPosX, board);
+    }
 
-    private int posX;
-    private int posY;
+    private boolean isKingMovement(int posY, int posX, int newPosY, int newPosX) {
+        return Math.abs(newPosX-posX) < 2 && Math.abs(newPosY-posY) < 2;
+    }
 
+    private boolean isSomethingInTheWay(int newPosY, int newPosX, Board board) {
+        return board[newPosY][newPosX].isEmpty();
+    }
 
-
-    public boolean isMoveValid(int posY, int posX, int newPosY, int newPosX){
-
-        return true;
+    private boolean isCastleValid(int posY, int posX, int newPosY, int newPosX, Board board) {
+        //todo ( newPosX-posX > 0 ? canCastleK : canCastleQ)
     }
 }
-
