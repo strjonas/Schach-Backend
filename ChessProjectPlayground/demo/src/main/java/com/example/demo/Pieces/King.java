1a401d9f0b0bf821d67ebc1a963a2672d83ff453
coSK123 <62240598+coSK123@users.noreply.github.com>
2023-05-04 22:52:37 +0200

    Better readability for King

--- ChessProjectPlayground/demo/src/main/java/com/example/demo/Pieces/King.java
+++ ChessProjectPlayground/demo/src/main/java/com/example/demo/Pieces/King.java
@@ -2,9 +2,9 @@ package com.example.demo.Pieces;
 
 import com.example.demo.Board;
 
-public class King extends Pieces{
-    private boolean canCastleQ;
-    private boolean canCastleK;
+public class King extends Pieces {
+    private final boolean canCastleQ;
+    private final boolean canCastleK;
 
     public King(boolean isBlack) {
         super(isBlack);
@@ -12,180 +12,192 @@ public class King extends Pieces{
         canCastleQ = true;
     }
 
-    public boolean isMoveValid(int posY, int posX, int newPosY, int newPosX, Board board){
+    public boolean isMoveValid(int posY, int posX, int newPosY, int newPosX, Board board) {
         return super.isMoveValid(posY, posX, newPosY, newPosX) && isKingMovement(posY, posX, newPosY, newPosX) && isSomethingInTheWay(newPosY, newPosX, board);
     }
 
     private boolean isKingMovement(int posY, int posX, int newPosY, int newPosX) {
-        return Math.abs(newPosX-posX) < 2 && Math.abs(newPosY-posY) < 2;
+        return Math.abs(newPosX - posX) < 2 && Math.abs(newPosY - posY) < 2;
     }
 
     private boolean isSomethingInTheWay(int newPosY, int newPosX, Board board) {
-        return board.isEmpty(newPosY,newPosX);
+        return board.isEmpty(newPosY, newPosX);
     }
 
     public boolean isCastleValid(int posY, int posX, int newPosX, Board board) {
         // todo !!!!!!!!!
-        if (newPosX-posX > 0 && canCastleK) {
+        if (newPosX - posX > 0 && canCastleK) {
             //kingside
-            if (board.getBoard()[posY][7] instanceof Rook && !((Rook) board.getBoard()[posY][7]).getHasMoved() && board.isEmpty(posY, posX+1) && board.isEmpty(posY, posX+2)
-                    && !isChecked(posY, posX+1, board, getIsBlack()) && !isChecked(posY, posX+2, board, getIsBlack())) {
-                return true;
-                //move rook
-            }
-        } else if (newPosX-posX < 0 && canCastleQ){
-            if (board.getBoard()[posY][0] instanceof Rook && !((Rook) board.getBoard()[posY][0]).getHasMoved() &&board.isEmpty(posY, posX-1) && board.isEmpty(posY, posX-2)
-                    && !isChecked(posY, posX-1, board, getIsBlack()) && !isChecked(posY, posX-2, board, getIsBlack())) {
-                return true;
-                //move rook
-            }
+            //move rook
+            return board.getBoard()[posY][7] instanceof Rook && !((Rook) board.getBoard()[posY][7]).getHasMoved() && board.isEmpty(posY, posX + 1) && board.isEmpty(posY, posX + 2)
+                    && !isChecked(posY, posX + 1, board, getIsBlack()) && !isChecked(posY, posX + 2, board, getIsBlack());
+        } else if (newPosX - posX < 0 && canCastleQ) {
+            //move rook
+            return board.getBoard()[posY][0] instanceof Rook && !((Rook) board.getBoard()[posY][0]).getHasMoved() && board.isEmpty(posY, posX - 1) && board.isEmpty(posY, posX - 2)
+                    && !isChecked(posY, posX - 1, board, getIsBlack()) && !isChecked(posY, posX - 2, board, getIsBlack());
             //queenside
         }
         //todo ( newPosX-posX > 0 ? canCastleK : canCastleQ)
         return false;
     }
 
-    public boolean isChecked(int posY, int posX, Board board, boolean isBlack){
+    public boolean isChecked(int posY, int posX, Board board, boolean isBlack) {
+
+        return isDiagonallyChecked(posY, posX, board, isBlack) || isHorizontallyOrVerticallyChecked(posY, posX, board, isBlack) || isCheckedByKnight(posY, posX, board, isBlack);
 
 
+    }
 
-        //Check for Rooks(and the Queen)
-        boolean isInPositiveYDirection = true;
-        boolean isInNegativeYDirection = true;
-        boolean isInNegativeXDirection = true;
-        boolean isInPositiveXDirection = true;
-        for(int i = 1;i<8;i++){
-            if(isInPositiveXDirection && posX+i <8 &&(board.getBoard()[posY][posX+i] instanceof Rook || board.getBoard()[posY][posX+i] instanceof Queen
-                    ||( i == 1 && board.getBoard()[posY][posX+i] instanceof King))
-                    && board.getBoard()[posY][posX+i].getIsBlack() != isBlack){
-                return true;
-            } else if(posX+i <8 && board.getBoard()[posY][posX+i] != null ){
-                 isInPositiveXDirection = false;
-            }
-
-            if(isInNegativeXDirection && posX-i >= 0&&(board.getBoard()[posY][posX-i] instanceof Rook || board.getBoard()[posY][posX-i] instanceof Queen
-                    ||( i == 1 && board.getBoard()[posY][posX-i] instanceof King))
-                    && board.getBoard()[posY][posX-i].getIsBlack() != isBlack){
-                return true;
-            } else if(posX-i >= 0 &&board.getBoard()[posY][posX-i] != null ){
-                isInNegativeXDirection = false;
-            }
-
-            if(isInPositiveYDirection && posY+i <8 &&(board.getBoard()[posY+1][posX] instanceof Rook || board.getBoard()[posY+1][posX] instanceof Queen
-                    ||( i == 1 && board.getBoard()[posY+i][posX] instanceof King))
-                    && board.getBoard()[posY+1][posX].getIsBlack() != isBlack){
-                return true;
-            } else if(posY+i <8 &&board.getBoard()[posY+1][posX] != null ){
-                isInPositiveYDirection = false;
-            }
-
-            if(isInNegativeYDirection && posY-i >= 0&&(board.getBoard()[posY-1][posX] instanceof Rook || board.getBoard()[posY-1][posX] instanceof Queen
-                    ||( i == 1 && board.getBoard()[posY-i][posX] instanceof King))
-                    && board.getBoard()[posY-1][posX].getIsBlack() != isBlack){
-                return true;
-            } else if(posY-i >= 0&& board.getBoard()[posY-1][posX] != null ){
-                isInNegativeYDirection = false;
-            }
-
-
-
+    private boolean isCheckedByKnight(int posY, int posX, Board board, boolean isBlack) {
+        if ((posY + 2 < 8 && posX + 1 < 8) && board.getBoard()[posY + 2][posX + 1] instanceof Knight && board.getBoard()[posY + 2][posX + 1].getIsBlack() != isBlack) {
+            return true;
         }
+        if ((posY + 2 < 8 && posX - 1 >= 0) && board.getBoard()[posY + 2][posX - 1] instanceof Knight && board.getBoard()[posY + 1][posX - 1].getIsBlack() != isBlack) {
+            return true;
+        }
+        if ((posY - 2 >= 0 && posX + 1 < 8) && board.getBoard()[posY - 2][posX + 1] instanceof Knight && board.getBoard()[posY - 2][posX + 1].getIsBlack() != isBlack) {
+            return true;
+        }
+        if ((posY - 2 >= 0 && posX - 1 >= 0) && board.getBoard()[posY - 2][posX - 1] instanceof Knight && board.getBoard()[posY - 2][posX - 1].getIsBlack() != isBlack) {
+            return true;
+        }
+        if ((posY + 1 < 8 && posX + 2 < 8) && board.getBoard()[posY + 1][posX + 2] instanceof Knight && board.getBoard()[posY + 1][posX + 2].getIsBlack() != isBlack) {
+            return true;
+        }
+        if ((posY + 1 < 8 && posX - 2 >= 0) && board.getBoard()[posY + 1][posX - 2] instanceof Knight && board.getBoard()[posY + 1][posX - 2].getIsBlack() != isBlack) {
+            return true;
+        }
+        if ((posY - 1 >= 0 && posX + 2 < 8) && board.getBoard()[posY - 1][posX + 2] instanceof Knight && board.getBoard()[posY - 1][posX + 2].getIsBlack() != isBlack) {
+            return true;
+        }
+        return (posY - 1 >= 0 && posX - 2 >= 0) && board.getBoard()[posY - 1][posX - 2] instanceof Knight && board.getBoard()[posY - 1][posX - 2].getIsBlack() != isBlack;
+    }
+
+    private boolean isDiagonallyChecked(int posY, int posX, Board board, boolean isBlack) {
         int i = 1;
-         boolean plusplus = false;
-         boolean plusminus = false;
-         boolean minusminus = false;
-         boolean minusplus = false;
+        boolean plusplus = false;
+        boolean plusminus = false;
+        boolean minusminus = false;
+        boolean minusplus = false;
         //check for Pawns and Bishops(and the Queen)
-        while(i <7) {
-            if(!isBlack) {
+        while (i < 7) {
+            if (!isBlack) {
                 if ((posY + i < 8 && posX + i < 8) && (posY + 1 == posY + i) &&
                         ((board.getBoard()[posY + i][posX - 1] instanceof Pawn && board.getBoard()[posY + i][posX - 1].getIsBlack()) ||
-                                (board.getBoard()[posY + i][posX + 1] instanceof Pawn  && board.getBoard()[posY + i][posX - 1].getIsBlack()))) {
+                                (board.getBoard()[posY + i][posX + 1] instanceof Pawn && board.getBoard()[posY + i][posX - 1].getIsBlack()))) {
                     return true;
                 }
-            }else{
+            } else {
                 if ((posY - i >= 0 && posX - i >= 0) && (posY - 1 == posY - i) &&
                         ((board.getBoard()[posY - i][posX - 1] instanceof Pawn && !board.getBoard()[posY - i][posX - 1].getIsBlack()) ||
-                        (board.getBoard()[posY - i][posX + 1] instanceof Pawn && !board.getBoard()[posY - i][posX - 1].getIsBlack()))) {
+                                (board.getBoard()[posY - i][posX + 1] instanceof Pawn && !board.getBoard()[posY - i][posX - 1].getIsBlack()))) {
                     return true;
                 }
             }
 
 
-            if (!plusplus && (posY + i < 8 && posX + i < 8) && board.getBoard()[posY + i][posX + i] != null ) {
-                if ((board.getBoard()[posY + i][posX + i] instanceof Bishop || board.getBoard()[posY + i][posX + i] instanceof Queen
-                        ||( i == 1 && board.getBoard()[posY+i][posX+i] instanceof King))
+            if (!plusplus && (posY + i < 8 && posX + i < 8) && board.getBoard()[posY + i][posX + i] != null) {
+                if ((isQueenOrBishop(posY + i, posX + i, board)
+                        || (i == 1 && board.getBoard()[posY + i][posX + i] instanceof King))
                         && board.getBoard()[posY + i][posX + i].getIsBlack() != isBlack) {
                     return true;
-                } else{
+                } else {
                     plusplus = true;
                 }
             }
 
-            if (!minusplus &&(posY - i >= 0 && posX + i < 8) && board.getBoard()[posY - i][posX + i] != null ) {
-                if ( (board.getBoard()[posY - i][posX + i] instanceof Bishop || board.getBoard()[posY - i][posX + i] instanceof Queen
-                        ||( i == 1 && board.getBoard()[posY-i][posX+i] instanceof King))
+            if (!minusplus && (posY - i >= 0 && posX + i < 8) && board.getBoard()[posY - i][posX + i] != null) {
+                if ((isQueenOrBishop(posY - 1, posX - 1, board)
+                        || (i == 1 && board.getBoard()[posY - i][posX + i] instanceof King))
                         && board.getBoard()[posY - i][posX + i].getIsBlack() != isBlack) {
                     return true;
-                }else{
+                } else {
                     minusplus = true;
                 }
             }
 
-            if (!plusminus &&(posY + i < 8 && posX - i >= 0) && board.getBoard()[posY + i][posX - i] != null ) {
-                if ( (board.getBoard()[posY + i][posX - i] instanceof Bishop || board.getBoard()[posY + i][posX - i] instanceof Queen
-                        ||( i == 1 && board.getBoard()[posY+i][posX-i] instanceof King))
+            if (!plusminus && (posY + i < 8 && posX - i >= 0) && board.getBoard()[posY + i][posX - i] != null) {
+                if ((isQueenOrBishop(posY + 1, posX - 1, board)
+                        || (i == 1 && board.getBoard()[posY + i][posX - i] instanceof King))
                         && board.getBoard()[posY + i][posX - i].getIsBlack() != isBlack) {
                     return true;
-                }else{
+                } else {
                     plusminus = true;
                 }
             }
-            if (!minusminus &&(posY - i >= 0 && posX - i >= 0) && board.getBoard()[posY - i][posX - i] != null ) {
-                if ( (board.getBoard()[posY - i][posX - i] instanceof Bishop || board.getBoard()[posY - i][posX - i] instanceof Queen
-                        ||( i == 1 && board.getBoard()[posY-i][posX-i] instanceof King))
+            if (!minusminus && (posY - i >= 0 && posX - i >= 0) && board.getBoard()[posY - i][posX - i] != null) {
+                if ((isQueenOrBishop(posY - 1, posX - 1, board)
+                        || (i == 1 && board.getBoard()[posY - i][posX - i] instanceof King))
                         && board.getBoard()[posY - i][posX - i].getIsBlack() != isBlack) {
                     return true;
-                }
-                else{
+                } else {
                     minusminus = true;
                 }
             }
             i++;
-            }
-
-        //check for Knights
-        if((posY+2 < 8 && posX+1 <8) &&board.getBoard()[posY+2][posX+1] instanceof Knight && board.getBoard()[posY+2][posX+1].getIsBlack() != isBlack){
-            return true;
-        }
-        if((posY+2 < 8 && posX-1 >=0) && board.getBoard()[posY+2][posX-1] instanceof Knight && board.getBoard()[posY+1][posX-1].getIsBlack() != isBlack){
-            return true;
-        }
-        if((posY-2 >= 0 && posX+1 <8) &&board.getBoard()[posY-2][posX+1] instanceof Knight && board.getBoard()[posY-2][posX+1].getIsBlack() != isBlack){
-            return true;
-        }
-        if((posY-2 >= 0 && posX-1 >=0) &&board.getBoard()[posY-2][posX-1] instanceof Knight && board.getBoard()[posY-2][posX-1].getIsBlack() != isBlack){
-            return true;
-        }
-        if((posY+1 < 8 && posX+2 <8) &&board.getBoard()[posY+1][posX+2] instanceof Knight && board.getBoard()[posY+1][posX+2].getIsBlack() != isBlack){
-            return true;
-        }
-        if((posY+1 < 8 && posX-2 >=0) &&board.getBoard()[posY+1][posX-2] instanceof Knight && board.getBoard()[posY+1][posX-2].getIsBlack() != isBlack){
-            return true;
-        }
-        if((posY-1 >= 0 && posX+2 <8) &&board.getBoard()[posY-1][posX+2] instanceof Knight && board.getBoard()[posY-1][posX+2].getIsBlack() != isBlack){
-            return true;
-        }
-        if((posY-1 >= 0 && posX-2>=0) &&board.getBoard()[posY-1][posX-2] instanceof Knight && board.getBoard()[posY-1][posX-2].getIsBlack() != isBlack){
-            return true;
         }
         return false;
-        }
+    }
 
-        @Override
-        public Character toChar() {
-            return getIsBlack() ? 'k' : 'K';
+
+    private boolean isHorizontallyOrVerticallyChecked(int posY, int posX, Board board, boolean isBlack) {
+        boolean isInPositiveYDirection = true;
+        boolean isInNegativeYDirection = true;
+        boolean isInNegativeXDirection = true;
+        boolean isInPositiveXDirection = true;
+        for (int i = 1; i < 8; i++) {
+            if (isInPositiveXDirection && posX + i < 8 && (isQueenOrRook(posY, posX + 1, board)
+                    || (i == 1 && board.getBoard()[posY][posX + i] instanceof King))
+                    && board.getBoard()[posY][posX + i].getIsBlack() != isBlack) {
+                return true;
+            } else if (posX + i < 8 && board.getBoard()[posY][posX + i] != null) {
+                isInPositiveXDirection = false;
+            }
+
+            if (isInNegativeXDirection && posX - i >= 0 && (isQueenOrRook(posY, posX - 1, board)
+                    || (i == 1 && board.getBoard()[posY][posX - i] instanceof King))
+                    && board.getBoard()[posY][posX - i].getIsBlack() != isBlack) {
+                return true;
+            } else if (posX - i >= 0 && board.getBoard()[posY][posX - i] != null) {
+                isInNegativeXDirection = false;
+            }
+
+            if (isInPositiveYDirection && posY + i < 8 && (isQueenOrRook(posY + 1, posX, board)
+                    || (i == 1 && board.getBoard()[posY + i][posX] instanceof King))
+                    && board.getBoard()[posY + 1][posX].getIsBlack() != isBlack) {
+                return true;
+            } else if (posY + i < 8 && board.getBoard()[posY + 1][posX] != null) {
+                isInPositiveYDirection = false;
+            }
+
+            if (isInNegativeYDirection && posY - i >= 0 && (isQueenOrRook(posY - 1, posX, board)
+                    || (i == 1 && board.getBoard()[posY - i][posX] instanceof King))
+                    && board.getBoard()[posY - 1][posX].getIsBlack() != isBlack) {
+                return true;
+            } else if (posY - i >= 0 && board.getBoard()[posY - 1][posX] != null) {
+                isInNegativeYDirection = false;
+            }
+
+
         }
+        return false;
+
 
     }
+
+    private boolean isQueenOrRook(int posY, int posX, Board board) {
+        return board.getBoard()[posY][posX] instanceof Queen || board.getBoard()[posY][posX] instanceof Rook;
+    }
+
+    private boolean isQueenOrBishop(int posY, int posX, Board board) {
+        return board.getBoard()[posY][posX] instanceof Queen || board.getBoard()[posY][posX] instanceof Bishop;
+    }
+
+
+    @Override
+    public Character toChar() {
+        return getIsBlack() ? 'k' : 'K';
+    }
+
+}
 
--- ChessProjectPlayground/demo/src/main/java/com/example/demo/Pieces/Rook.java
+++ ChessProjectPlayground/demo/src/main/java/com/example/demo/Pieces/Rook.java
@@ -14,7 +14,7 @@ public class Rook extends Pieces{
     public boolean isMoveValid(int posY, int posX, int newPosY, int newPosX){
         return super.isMoveValid(posY,posX,newPosY,newPosX) && isRookMovement( posY, posX,  newPosY,  newPosX) ;
     }
-    public boolean isRookMovement(int posY, int posX, int newPosY, int newPosX){
+    private boolean isRookMovement(int posY, int posX, int newPosY, int newPosX){
         return (posY == newPosY && posX != newPosX) || (posY != newPosY && posX == newPosX);
     }
 
